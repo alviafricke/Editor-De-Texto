252145107b8b0ee95bc91f537cac5cd180069c2c
Alvaro Fricke <alvifricke@hotmail.com>
2022-03-03 18:51:46 -0300

    Inicio de proyecto

---
+++ src/Grafica/Ventana.java
@@ -0,0 +1,18 @@
+package Grafica;
+
+import javax.swing.JFrame;
+
+public class Ventana {
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		
+		frame.setBounds(100, 100, 100, 100);
+		frame.setVisible(true);
+		
+		
+	}
+	
+
+}
